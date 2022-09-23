1f8434b587f6bab677ded530f819774d3de49323
Matt Hipsey <matt.hipsey@uwa.edu.au>
2022-09-23 23:26:49 +0800

    update oasim

--- src/aed_oasim.F90
+++ src/aed_oasim.F90
@@ -108,6 +108,7 @@ MODULE aed_oasim
       INTEGER :: l490_l
       INTEGER :: spectral_output
       LOGICAL :: save_Kd
+      AED_REAL :: cloud, airpres, lwp, O3, WV, visibility, air_mass_type
 
     CONTAINS
          PROCEDURE :: define            => aed_define_oasim
@@ -131,47 +132,12 @@ MODULE aed_oasim
 !
 !
 
-   INTEGER :: nlambda_astm
-   INTEGER :: nlambda_w
+!   INTEGER :: nlambda_astm
+!   INTEGER :: nlambda_w
 
-   AED_REAL,DIMENSION(:),ALLOCATABLE :: a_w, b_w, et_astm, lambda_astm, lambda_w
-AED_REAL, parameter :: a_o_oasim(33) = (/ &
-      8.088360e+01, 3.584000e-01, 1.600000e-03, 1.000000e-04, 0.000000e+00, 8.000000e-04, 4.000000e-03, 1.210000e-02, 2.790000e-02, 5.290000e-02, &
-      8.320000e-02, 1.140000e-01, 1.176000e-01, 9.670000e-02, 6.430000e-02, 4.160000e-02, 2.610000e-02, 1.320000e-02, 7.200000e-03, 2.000000e-03, &
-      0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, &
-      0.000000e+00, 0.000000e+00, 0.000000e+00 &
-   /)
-AED_REAL, parameter :: a_u_oasim(33) = (/ &
-      0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, &
-      0.000000e+00, 0.000000e+00, 0.000000e+00, 5.800000e-03, 0.000000e+00, 2.333000e-01, 1.299000e-01, 0.000000e+00, 1.117600e+00, 1.000000e-04, &
-      0.000000e+00, 4.000000e-04, 0.000000e+00, 3.300000e-03, 3.000000e-04, 2.300000e-03, 1.400000e-03, 1.000000e-03, 1.000000e-04, 8.700000e-03, &
-      1.180000e-02, 2.560000e-02, 3.000000e-04 &
-   /)
-AED_REAL, parameter :: a_v_oasim(33) = (/ &
-      0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, 0.000000e+00, &
-      0.000000e+00, 1.480000e-02, 8.510000e-02, 1.600000e-03, 4.080000e-02, 9.000000e-04, 1.074000e-01, 1.462000e-01, 6.500000e-03, 5.680000e-02, &
-      4.914200e+00, 2.790000e-02, 1.759020e+01, 4.790000e-02, 1.153197e+02, 9.520310e+01, 3.640000e-02, 8.200000e-03, 2.000800e+00, 2.289211e+02, &
-      2.550000e-01, 3.147111e+02, 9.158000e-01 &
-   /)
-AED_REAL, parameter :: ET_oasim(33) = (/ &
-      1.652800e+01, 4.100350e+01, 1.279150e+01, 2.711800e+01, 3.519350e+01, 4.217950e+01, 4.919400e+01, 4.977000e+01, 4.839450e+01, 4.649350e+01, &
-      4.651600e+01, 4.613800e+01, 4.412300e+01, 4.204680e+01, 3.952450e+01, 3.787300e+01, 1.874650e+01, 6.687750e+01, 5.999000e+01, 1.022358e+02, &
-      8.478990e+01, 6.787720e+01, 5.607620e+01, 4.611060e+01, 3.737800e+01, 3.183130e+01, 2.725240e+01, 2.276910e+01, 1.861440e+01, 2.795210e+01, &
-      3.363000e+01, 3.257750e+01, 7.263500e+00 &
-   /)
-AED_REAL, parameter :: lambda_oasim(33) = (/ &
-      2.500000e+02, 3.250000e+02, 3.500000e+02, 3.750000e+02, 4.000000e+02, 4.250000e+02, 4.500000e+02, 4.750000e+02, 5.000000e+02, 5.250000e+02, &
-      5.500000e+02, 5.750000e+02, 6.000000e+02, 6.250000e+02, 6.500000e+02, 6.750000e+02, 7.000000e+02, 7.250000e+02, 7.750000e+02, 8.500000e+02, &
-      9.500000e+02, 1.050000e+03, 1.150000e+03, 1.250000e+03, 1.350000e+03, 1.450000e+03, 1.550000e+03, 1.650000e+03, 1.750000e+03, 1.900000e+03, &
-      2.200000e+03, 2.900000e+03, 3.700000e+03 &
-   /)
-integer, parameter :: nlambda_oasim = 33
-AED_REAL, parameter :: tau_r_oasim(33) = (/ &
-      2.822900e+00, 8.742000e-01, 5.925000e-01, 4.768000e-01, 3.643000e-01, 2.833000e-01, 2.238000e-01, 1.791000e-01, 1.451000e-01, 1.189000e-01, &
-      9.830000e-02, 8.200000e-02, 6.900000e-02, 5.840000e-02, 4.980000e-02, 4.270000e-02, 3.820000e-02, 3.190000e-02, 2.440000e-02, 1.680000e-02, &
-      1.080000e-02, 7.200000e-03, 5.000000e-03, 3.200000e-03, 2.600000e-03, 2.000000e-03, 1.500000e-03, 1.200000e-03, 9.000000e-04, 7.000000e-04, &
-      4.000000e-04, 1.000000e-04, 0.000000e+00 &
-   /)
+!  AED_REAL,DIMENSION(:),ALLOCATABLE :: a_w, b_w, et_astm, lambda_astm, lambda_w
+
+#include "oasim.inc"
 
 
 !===============================================================================
@@ -242,6 +208,7 @@ SUBROUTINE aed_define_oasim(data, namlst)
    INTEGER  :: lambda(100)
    LOGICAL  :: save_Kd = .FALSE.    ! compute attenuation, default=.false.
    LOGICAL  :: compute_mean_wind = .TRUE.
+   AED_REAL :: cloud, airpres, lwp, O3, WV, visibility, air_mass_type
 ! %% From Module Globals
 !  INTEGER  :: diag_level = 10      ! 0 = no diagnostic outputs
 !                                   ! 1 = basic diagnostic outputs
@@ -252,6 +219,7 @@ SUBROUTINE aed_define_oasim(data, namlst)
 
    NAMELIST /aed_oasim/ lambda_method, nlambda, lambda_min, lambda_max,        &
                         n_iop, iop_type, spectral_output, lambda, save_Kd,     &
+                        cloud, airpres, lwp, O3, WV, visibility, air_mass_type,&
                         compute_mean_wind, diag_level
 
 !
@@ -259,15 +227,23 @@ SUBROUTINE aed_define_oasim(data, namlst)
 !BEGIN
    print *,"        aed_oasim configuration"
 
-   ALLOCATE(a_w(33))
-   ALLOCATE(b_w(33))
-   ALLOCATE(et_astm(33))
-   ALLOCATE(lambda_astm(33))
-   ALLOCATE(lambda_w(33))
+ !  ALLOCATE(a_w(33))
+ !  ALLOCATE(b_w(33))
+ !  ALLOCATE(et_astm(33))
+ !  ALLOCATE(lambda_astm(33))
+ !  ALLOCATE(lambda_w(33))
 
    ! Read the namelist
    read(namlst,nml=aed_oasim,iostat=status)
    IF (status /= 0) STOP 'Error reading namelist aed_oasim'
+
+   data%cloud = cloud
+   data%airpres = airpres
+   data%lwp = lwp
+   data%O3 = O3
+   data%WV = WV
+   data%visibility = visibility
+   data%air_mass_type = air_mass_type
 
    data%nlambda = nlambda
    SELECT CASE (lambda_method)
@@ -292,28 +268,28 @@ SUBROUTINE aed_define_oasim(data, namlst)
 
       SELECT CASE (iop_type)
       CASE (1) ! diatoms
-!CAB         CALL interp(size(lambda_diatoms), lambda_diatoms, a_diatoms, nlambda, data%lambda, data%iops(i_iop)%a)
-!CAB         CALL interp(size(lambda_diatoms), lambda_diatoms, b_diatoms, nlambda, data%lambda, data%iops(i_iop)%b)
+         CALL interp(size(lambda_diatoms), lambda_diatoms, a_diatoms, nlambda, data%lambda, data%iops(i_iop)%a)
+         CALL interp(size(lambda_diatoms), lambda_diatoms, b_diatoms, nlambda, data%lambda, data%iops(i_iop)%b)
          data%iops(i_iop)%b_b = 0.002 ! Gregg & Rousseau 2016 but originally Morel 1988
       CASE (2) ! chlorophytes
-!CAB         CALL interp(size(lambda_chlorophytes), lambda_chlorophytes, a_chlorophytes, nlambda, data%lambda, data%iops(i_iop)%a)
-!CAB         CALL interp(size(lambda_chlorophytes), lambda_chlorophytes, b_chlorophytes, nlambda, data%lambda, data%iops(i_iop)%b)
+         CALL interp(size(lambda_chlorophytes), lambda_chlorophytes, a_chlorophytes, nlambda, data%lambda, data%iops(i_iop)%a)
+         CALL interp(size(lambda_chlorophytes), lambda_chlorophytes, b_chlorophytes, nlambda, data%lambda, data%iops(i_iop)%b)
          data%iops(i_iop)%b_b = 0.00071 * 10 ! Note: 10x Ahn et al. 1992 as reported in Gregg & Rousseau 2016
       CASE (3) ! cyanobacteria
-!CAB         CALL interp(size(lambda_cyanobacteria), lambda_cyanobacteria, a_cyanobacteria, nlambda, data%lambda, data%iops(i_iop)%a)
-!CAB         CALL interp(size(lambda_cyanobacteria), lambda_cyanobacteria, b_cyanobacteria, nlambda, data%lambda, data%iops(i_iop)%b)
+         CALL interp(size(lambda_cyanobacteria), lambda_cyanobacteria, a_cyanobacteria, nlambda, data%lambda, data%iops(i_iop)%a)
+         CALL interp(size(lambda_cyanobacteria), lambda_cyanobacteria, b_cyanobacteria, nlambda, data%lambda, data%iops(i_iop)%b)
          data%iops(i_iop)%b_b = 0.0032 ! Gregg & Rousseau 2016 but originally Ahn et al. 1992
       CASE (4) ! coccolithophorids
-!CAB         CALL interp(size(lambda_coccolithophores), lambda_coccolithophores, a_coccolithophores, &
-!CAB                                                                             nlambda, data%lambda, data%iops(i_iop)%a)
-!CAB         CALL interp(size(lambda_coccolithophores), lambda_coccolithophores, b_coccolithophores, &
-!CAB                                                                             nlambda, data%lambda, data%iops(i_iop)%b)
+         CALL interp(size(lambda_coccolithophores), lambda_coccolithophores, a_coccolithophores, &
+                                                                             nlambda, data%lambda, data%iops(i_iop)%a)
+         CALL interp(size(lambda_coccolithophores), lambda_coccolithophores, b_coccolithophores, &
+                                                                             nlambda, data%lambda, data%iops(i_iop)%b)
          data%iops(i_iop)%b_b = 0.00071 * 10 ! Note: 10x Morel 1988 as reported in Gregg & Rousseau 2016
       CASE (5) ! dinoflagellates
-!CAB         CALL interp(size(lambda_dinoflagellates), lambda_dinoflagellates, a_dinoflagellates, &
-!CAB                                                                             nlambda, data%lambda, data%iops(i_iop)%a)
-!CAB         CALL interp(size(lambda_dinoflagellates), lambda_dinoflagellates, b_dinoflagellates, &
-!CAB                                                                             nlambda, data%lambda, data%iops(i_iop)%b)
+         CALL interp(size(lambda_dinoflagellates), lambda_dinoflagellates, a_dinoflagellates, &
+                                                                             nlambda, data%lambda, data%iops(i_iop)%a)
+         CALL interp(size(lambda_dinoflagellates), lambda_dinoflagellates, b_dinoflagellates, &
+                                                                             nlambda, data%lambda, data%iops(i_iop)%b)
          data%iops(i_iop)%b_b = 0.0029 ! Gregg & Rousseau 2016 but originally Morel 1988
       CASE (6) ! detritus (small, as described in Gregg & Rousseau 2016)
          ! Parameters below match the small organic detritus parametrization of Gallegos et al. 2011 (table 2)
@@ -391,7 +367,21 @@ SUBROUTINE aed_define_oasim(data, namlst)
 
    data%id_lon = aed_locate_global('longitude')
    data%id_lat = aed_locate_global('latitude')
-   data%id_wind_speed = aed_locate_global('wind_speed')
+  ! data%id_yearday = aed_locate_global('yearday')
+
+   data%id_h = aed_locate_global('layer_ht')
+   data%id_wind_speed = aed_locate_sheet_global('wind_speed')
+   data%id_relhum = aed_locate_sheet_global('humidity')
+  
+  ! data%id_cloud = aed_locate_sheet_global('cloud')
+  ! data%id_airpres = aed_locate_sheet_global('air_press')
+  ! data%id_lwp = aed_locate_sheet_global('air_press')
+  ! data%id_O3 = aed_locate_sheet_global('air_press')
+  ! data%id_WV = aed_locate_sheet_global('air_press')
+  ! data%id_visibility = aed_locate_sheet_global('air_press')
+  ! data%id_air_mass_type = aed_locate_sheet_global('air_press')
+
+
 
 ! MAKE PARAM CAB ?
 !  data%id_cloud = aed_locate_sheet_global('cloud_area_fraction')
@@ -402,12 +392,14 @@ SUBROUTINE aed_define_oasim(data, namlst)
 ! MAKE PARAM CAB ?
 !  data%id_yearday = aed_locate_sheet_global('number_of_days_since_start_of_the_year')
    data%id_yearday = aed_define_sheet_diag_variable('number_of_days_since_start_of_the_year', '-', '-')
-! MAKE PARAM CAB ?
+
+! MAKE PARAM CAB ? MH: made env var
 !  data%id_h = aed_locate_global('cell_thickness')
-   data%id_h = aed_define_diag_variable('cell_thickness', '-', '-')
-! MAKE PARAM CAB ?
+!   data%id_h = aed_define_diag_variable('cell_thickness', '-', '-')
+! MAKE PARAM CAB ? MH: made env var
 !  data%id_relhum = aed_locate_sheet_global('relative_humidity')
-   data%id_relhum = aed_define_sheet_diag_variable('relative_humidity', '-', '-')
+!   data%id_relhum = aed_define_sheet_diag_variable('relative_humidity', '-', '-')
+
 ! MAKE PARAM CAB ?
 !  data%id_lwp = aed_locate_sheet_global('atmosphere_mass_content_of_cloud_liquid_water')
    data%id_lwp = aed_define_sheet_diag_variable('atmosphere_mass_content_of_cloud_liquid_water', '-', '-')
@@ -613,6 +605,7 @@ SUBROUTINE aed_calculate_oasim(data,column,layer_idx)
    INTEGER  :: i_iop
    AED_REAL :: c_iop, h, swr_top, costheta_r, dir_frac
 
+   
 !-------------------------------------------------------------------------------
 !BEGIN
    IF (data%spectral_output == 2) ALLOCATE(spectrum_out(size(data%lambda_out)))
@@ -620,16 +613,19 @@ SUBROUTINE aed_calculate_oasim(data,column,layer_idx)
    longitude   = _STATE_VAR_S_(data%id_lon)
    latitude    = _STATE_VAR_S_(data%id_lat)
    yearday     = _STATE_VAR_S_(data%id_yearday)
-   cloud_cover = _STATE_VAR_S_(data%id_cloud)           ! Cloud cover (fraction, 0-1)
    wind_speed  = _STATE_VAR_S_(data%id_wind_speed)      ! Wind speed @ 10 m above surface (m/s)
-   airpres     = _STATE_VAR_S_(data%id_airpres)         ! Surface air pressure (Pa)
    relhum      = _STATE_VAR_S_(data%id_relhum)          ! Relative humidity (-)
-   LWP         = _STATE_VAR_S_(data%id_lwp)             ! Cloud liquid water content (kg m-2)
-   O3          = _STATE_VAR_S_(data%id_O3)              ! Ozone content (kg m-2)
-   water_vapour= _STATE_VAR_S_(data%id_wv)              ! Total precipitable water vapour (kg m-2) - equivalent to mm
+
+   airpres     = data%airpres ! _STATE_VAR_S_(data%id_airpres)         ! Surface air pressure (Pa)
+   cloud_cover = data%cloud ! _STATE_VAR_S_(data%id_cloud)           ! Cloud cover (fraction, 0-1)
+   LWP         = data%LWP !_STATE_VAR_S_(data%id_lwp)             ! Cloud liquid water content (kg m-2)
+   O3          = data%O3 !_STATE_VAR_S_(data%id_O3)              ! Ozone content (kg m-2)
+   water_vapour= data%WV ! _STATE_VAR_S_(data%id_wv)              ! Total precipitable water vapour (kg m-2) - equivalent to mm
+   visibility  = data%visibility ! _STATE_VAR_S_(data%id_visibility)      ! Visibility (m)
+   AM          = data%air_mass_type !_STATE_VAR_S_(data%id_air_mass_type)   ! Aerosol air mass type (1: open ocean, 10: continental)
    WM          =  _DIAG_VAR_S_(data%id_mean_wind_speed) ! Daily mean wind speed @ 10 m above surface (m/s)
-   visibility  = _STATE_VAR_S_(data%id_visibility)      ! Visibility (m)
-   AM          = _STATE_VAR_S_(data%id_air_mass_type)   ! Aerosol air mass type (1: open ocean, 10: continental)
+
+  ! cloud, airpres, lwp, O3, WV, visibility, air_mass_type,&
 
    ! For debugging
    _DIAG_VAR_S_(data%id_wind_out) = wind_speed
